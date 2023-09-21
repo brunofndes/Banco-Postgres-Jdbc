f426ead9dd9f255bc03ad5d53eed332ebdb73c5d
Bruno Fernandes <brunof900@gmail.com>
2023-09-21 05:38:36 -0300

    Adicionando metodos sql de buscar e atualizar

--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -3,6 +3,9 @@ package pos_java_jdbc.pos_java_jdbc;
 
 
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.junit.Test;
 
 import conexaojdbc.SingleConenection;
@@ -17,10 +20,59 @@ public class TesteBancoJdbc{
 		UserPosDao userPosDao = new UserPosDao();
 		Userposjava userposjava = new Userposjava();
 		
-		userposjava.setId(4L);
-		userposjava.setNome("Teste de função");
-		userposjava.setEmail("teste do teste");
+	
 		
-		userPosDao.salvar(userposjava);
+		
 	}
+	
+	@Test
+	public void initListar() {
+		UserPosDao dao = new UserPosDao();
+		
+		try {
+			List<Userposjava> list = dao.listar();
+			
+			for (Userposjava userposjava : list) {
+				System.out.println(userposjava);
+				System.out.println("----------------------------------------------------");
+			}
+			
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void initBuscar() {
+		UserPosDao dao = new UserPosDao();
+		
+		try {
+			Userposjava userposjava = dao.buscar(4L);
+			
+			System.out.println("CONSULTA POR ID "+ userposjava);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void initAtualizar() {
+		
+		try {
+			UserPosDao dao = new UserPosDao();
+			
+			Userposjava objetoBanco = dao.buscar(3L);
+			objetoBanco.setNome("ATUALIZADO AGORA");
+			
+			dao.atualizar(objetoBanco);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
